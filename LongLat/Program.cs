d2bb788f1c6ad8cf4058409cce1f1d79c0ee5260
GamehunterKaan <68380968+GamehunterKaan@users.noreply.github.com>
2022-03-28 20:23:09 +0300

    Add files via upload

---
+++ LongLat/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Device.Location;
+
+
+namespace LongLat
+{
+    static class Program
+    {
+        static void Main()
+        {
+            GeoCoordinate geoCoordinate = new GeoCoordinate();
+            if (geoCoordinate.IsUnknown)
+            {
+                Console.WriteLine("[-] GeoCoordinate info is not available!");
+            }
+            else
+            {
+                Console.WriteLine("[+] {0}", geoCoordinate.Latitude);
+                Console.WriteLine("[+] {0}", geoCoordinate.Longitude);
+            }
+        }
+    }
+}
